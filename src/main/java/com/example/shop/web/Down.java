2a327e07e2f33b14962b81c9caa2873682929f6d
574 <2659332353@qq.com>
2021-05-15 20:58:57 +0800

    v1.1.4下载功能

--- src/main/java/com/example/shop/web/Down.java
+++ src/main/java/com/example/shop/web/Down.java
@@ -1,39 +1,36 @@
 package com.example.shop.web;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletOutputStream;
-import jakarta.servlet.http.*;
 
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Download extends HttpServlet {
+public class Down extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         down(request, response);
-
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         down(request, response);
-
     }
 
     private void down(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String fileName = request.getParameter("fileName");
-        ServletContext servletContext =request.getServletContext();
-        String filePath = "/file/" + fileName;
+        String filePath = "D:\\IdeaProgram\\shop\\src\\main\\webapp\\file\\" + request.getParameter("fileName");
+        ServletContext servletContext = request.getServletContext();
         response.setContentType(servletContext.getMimeType(filePath));
-
-        InputStream resourceStream = servletContext.getResourceAsStream(fileName);
+        InputStream resourceAsStream = servletContext.getResourceAsStream(filePath);
         ServletOutputStream outputStream = response.getOutputStream();
-        byte[] buff = new byte[102400];
+        byte[] buff = new byte[10240];
         int len;
-        while ((len = resourceStream.read(buff)) != -1) {
-            outputStream.write(buff,0,len);
+        while ((len = resourceAsStream.read(buff)) != -1) {
+            outputStream.write(buff, 0, len);
         }
-
-
     }
 }
